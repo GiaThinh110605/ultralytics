67627024d136291782e0141a720f976f9418c526
sjsjsmsmsj <lamgiathinh05@gmail.com>
2026-08-07 15:08:37 +0700

    apply eca as same as with author

--- ultralytics/nn/modules/eca.py
+++ ultralytics/nn/modules/eca.py
@@ -1,42 +1,29 @@
-import math
 import torch
-import torch.nn as nn
-
+from torch import nn
+from torch.nn.parameter import Parameter
 
 class ECA(nn.Module):
+    """Constructs a ECA module.
+
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
     """
-    Efficient Channel Attention (CVPR 2020)
-    """
-
-    def __init__(self, channels, gamma=2, b=1):
-        super().__init__()
-
-        t = int(abs((math.log2(channels) + b) / gamma))
-        k = t if t % 2 else t + 1
-        k = max(k, 3)
-
+    def __init__(self, channel, k_size=3):
+        super(ECA, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.conv = nn.Conv1d(
-            1,
-            1,
-            kernel_size=k,
-            padding=(k - 1) // 2,
-            bias=False
-        )
-
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-
+        # feature descriptor on the global spatial information
         y = self.avg_pool(x)
 
-        y = y.squeeze(-1).transpose(-1, -2)
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
 
-        y = self.conv(y)
-
-        y = y.transpose(-1, -2).unsqueeze(-1)
-
+        # Multi-scale information fusion
         y = self.sigmoid(y)
 
         return x * y.expand_as(x)
+        
